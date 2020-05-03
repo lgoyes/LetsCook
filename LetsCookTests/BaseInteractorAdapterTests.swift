fba66783d6135130561e5f50554c06e606f189a9
Luis David Goyes Garces <luis.goyes117@gmail.com>
2020-05-03 16:18:57 -0500

    BaseInteractorAdapterTests

--- LetsCookTests/BaseInteractorAdapterTests.swift
+++ LetsCookTests/BaseInteractorAdapterTests.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2020 LuisGoyes. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import RxSwift
+@testable import LetsCook
+
+class TestBaseInteractorAdapter<Response, Params>: BaseInteractorAdapter<Response, Params, TestBaseInteractorAdapter> {
+    
+}
+
+class BaseInteractorAdapterTests: XCTestCase {
+    
+    struct Constants {
+        enum TestError: Error {
+            case dummyError
+        }
+    }
+    
+    var sut: TestBaseInteractorAdapter<Any, Any>!
+    
+    override func setUp() {
+        super.setUp()
+        sut = TestBaseInteractorAdapter<Any, Any>()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testParams_givenParamsAttribute_setParamsToAdaptedInteractor() {
+        let dummyParams = 1
+        sut.params(dummyParams)
+        
+        XCTAssertEqual(sut.baseInteractor.paramsAttribute! as! Int, dummyParams)
+    }
+    
+    func testParams_givenParamsAttribute_returnAdaptedInteractor() {
+        let dummyParams = 1
+        let chainedInteractor = sut.params(dummyParams)
+        
+        XCTAssert(chainedInteractor === sut)
+    }
+    
+    func testOnFinallyCallback_givenOnFinallyCallback_setOnFinallyToAdaptedInteractor() {
+        let expectation = XCTestExpectation()
+        let onFinallyCallback: (() -> Void) = {
+            expectation.fulfill()
+        }
+        sut.onFinally(onFinallyCallback)
+        sut.baseInteractor.onFinallyCallback?()
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testOnFinallyCallback_givenOnFinallyCallback_returnAdaptedInteractor() {
+        let onFinallyCallback: (() -> Void) = {}
+        let chainedInteractor = sut.onFinally(onFinallyCallback)
+        
+        XCTAssert(chainedInteractor === sut)
+    }
+}
